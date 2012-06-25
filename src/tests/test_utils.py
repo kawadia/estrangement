74ba699d6a47608b412279ef15d099bfcc90ead1
dabideen <dabideen@bbn.com>
2012-06-25 16:09:33 -0400

    Added documentation to utils and created node unittests for the included functions.

---
+++ src/tests/test_utils.py
@@ -0,0 +1,55 @@
+import networkx as nx
+import sys
+import os
+
+sys.path.append(os.getcwd() + "/..")
+import utils
+
+class test_utils:
+	def setUp(self):
+	        self.g0 = nx.Graph()
+        	self.g1 = nx.Graph()
+		self.g2 = nx.Graph()
+		self.g3 = nx.Graph()
+		self.g4 = nx.Graph()
+		self.g5 = nx.Graph()
+        	self.g0.add_edges_from([(1,2,{'weight':2}),(1,3,{'weight':1}),(2,4,{'weight':1})])
+        	self.g1.add_edges_from([(1,4,{'weight':1}),(2,3,{'weight':1}),(3,4,{'weight':1})])
+        	self.g2.add_edges_from([(1,2,{'weight':2}),(2,3,{'weight':1}),(3,4,{'weight':1})])
+		self.g3.add_edges_from([(5,6),(5,7)])
+		self.g4.add_edges_from([(1,5),(2,3)])	
+        	self.g5.add_edges_from([(1,2,{'weight':2}),(1,3,{'weight':1}),(2,4,{'weight':1})])
+		self.label_dict1 = {1:'a',2:'a',3:'b',4:'b',5:'c',6:'c'}	
+		self.label_dict2 = {1:'a',2:'b',3:'b',4:'b',5:'c',6:'c'}	
+		self.label_dict3 = {1:'a',2:'b',3:'c',4:'d',5:'e',6:'f'}	
+		self.label_dict4 = {1:'a',2:'a',3:'a',4:'a',5:'a',6:'a'}	
+		self.label_dict5 = {1:'b',2:'b',3:'b',4:'b',5:'b',6:'b'}
+
+	def test_graph_distance(self):
+    		assert utils.graph_distance(self.g0, self.g1) == 1
+    		assert utils.graph_distance(self.g0, self.g1, False) == 1
+    		assert utils.graph_distance(self.g0, self.g0) == 0
+    		assert utils.graph_distance(self.g0, self.g0) == 0
+		assert utils.graph_distance(self.g0, self.g2, False) == 0.8
+		assert utils.graph_distance(self.g0, self.g2, True) == 0.5
+
+	def test_node_graph_distance(self):
+                assert utils.node_graph_distance(self.g0, self.g1) == 0
+                assert utils.node_graph_distance(self.g0, self.g0) == 0
+                assert utils.node_graph_distance(self.g0, self.g3) == 1
+                assert utils.node_graph_distance(self.g0, self.g4) == 0.4
+                   
+	def test_Estrangement(self):
+		assert utils.Estrangement1(self.g0, self.label_dict1, self.g2, True) == 0     # one common edge, same community
+		assert utils.Estrangement1(self.g0, self.label_dict2, self.g2, True) == 0.5   # one common edge, diff community
+		assert utils.Estrangement1(self.g0, self.label_dict4, self.g5, True) == 0     # all common edge, same community
+		assert utils.Estrangement1(self.g0, self.label_dict3, self.g5, True) == 1     # all common edge, all diff community
+		assert utils.Estrangement1(self.g0, self.label_dict1, self.g2, False) == 0.25 # one edge between community
+		assert utils.Estrangement1(self.g0, self.label_dict4, self.g2, False) == 0    # zero edge between community
+		assert utils.Estrangement1(self.g0, self.label_dict3, self.g2, False) == 1    # all edges between community
+
+	def test_match_labels(self):
+		assert utils.match_labels(self.label_dict1, self.label_dict1) == self.label_dict1  # snapshots are the same
+                assert utils.match_labels(lself.abel_dict5, self.label_dict4) == self.label_dict4  # same community, diff label
+                assert utils.match_labels(lself.abel_dict4, self.label_dict4) == self.label_dict4  # same community, same label
+
--- src/utils.py
+++ src/utils.py
@@ -63,7 +63,7 @@ def graph_distance(g0, g1, weighted=True):
     return graph_distance
 
 def node_graph_distance(g0, g1):
-    """Return the Tanimoto distance between the two input graphs.
+    """Return the Jaccard distance between the two input graphs.
 
     Jaccard distance between the set of nodes is defined as    
     (a.b - (aUb - a.b)) /aUb where a.b is dot product and aUb = a^2 + b^2 - a.b
@@ -89,7 +89,7 @@ def node_graph_distance(g0, g1):
     
     return graph_distance
 
-def Estrangement(G, label_dict, Zgraph, gap_proof):
+def Estrangement1(G, label_dict, Zgraph, gap_proof):
     """Return the Estrangement between G and Zgraph
 
     Compute Q-tauE for the given input parameters
@@ -97,13 +97,14 @@ def Estrangement(G, label_dict, Zgraph, gap_proof):
     Parameters
     -----------
     G: graph
-	A networkx graph object
+	A networkx graph object (new graph)
     label_dict: dictionary
 	key = node_identifier, value = community label
     ZGraph: graph
-	<>
+	A networkx graph object (old graph)
     gap_proof: boolean
-	<>
+	If True, consider only edges common to G and ZGraph
+	else consider all edges in ZGraph
 
     Returns
     -------
@@ -130,84 +131,69 @@ def Estrangement(G, label_dict, Zgraph, gap_proof):
     return estrangement
 
 
-
-
 def match_labels(label_dict, prev_label_dict):
-    """ match labels using Sameet's bipartitie graph based algorithm
+    """Returns a list of community labels to be preserved representing the 
+    communities that remain mostly intact between snapshots.
 
     We start by representing the communities at t-1 and t as nodes of a
-    bipartite graph.
+    bipartite graph. From each node at t-1 draw a directed link to the 
+    node at t with which it has maximum overlap. From each node at t draw 
+    a directed link to the node at t-1 with which it has maximum overlap.
 
-    from each node at t-1 draw a directed link to the node at t with which it has
-    maximum overlap.
-
-    from each node at t draw a directed link to the node at t-1 with which it has
-    maximum overlap.
-
-        Basically x,y and z choose who they are most similar to among a and b
+    Basically x,y and z choose who they are most similar to among a and b
     and denote this by arrows directed outward from them. Similarly a and
     b, choose who they are most similar to among x, y and z. Then the rule
     is that labels on the t-1 side of every bidirected (symmetric) link is
     preserved - all other labels on the t-1 side die.
 
+    Parameters
+    ----------
+    label_dict: dictionary
+	{node:community} at time t
+    prev_label_dict: dictionary
+	{node:community} at time (t - 1)
+
+    Returns
+    -------
+    matched_label_dict: dictionary
+  	{node:community} new labelling
     """
     
     # corner case for the first snapshot
     if prev_label_dict == {}:
         return label_dict
 
-    nodesets_per_label_t = collections.defaultdict(set) # key = label, val = set
-                                                    # of nodes with that label
-
-    nodesets_per_label_t_minus_1 = collections.defaultdict(set) # key = label, val = set
-                                                    # of nodes with that label
+    nodesets_per_label_t = collections.defaultdict(set) 
+    nodesets_per_label_t_minus_1 = collections.defaultdict(set) 
+    # key = label, val = set of nodes with that label
     
     for n,l in label_dict.items():
         nodesets_per_label_t[l].add(n)
 
-
     for n,l in prev_label_dict.items():
         nodesets_per_label_t_minus_1[l].add(n)
 
-
-    #logging.debug("nodesets_per_label_t_minus_1: %s",
-    #    str(nodesets_per_label_t_minus_1))
-    #logging.debug("nodesets_per_label_t: %s", str(nodesets_per_label_t))
-
     overlap_dict = {} # key = (prev_label, new_label), value = jaccard overlap
 
-    overlap_graph = nx.Graph() # store jaccard overlap between all pairs of
-    # labels between t and t-1. Undirected bi-partite graph
-    # compute jaccard between all possible directed pairs of labels between
-    # snapshopts t and t-1
+    overlap_graph = nx.Graph() 
+    # Undirected bi-partite graph with the vertices being the labels and
+    # the weight being the jaccard distance between them in t and (t-1) 
+
     for l_t, nodeset_t in nodesets_per_label_t.items():
         for l_t_minus_1, nodeset_t_minus_1 in nodesets_per_label_t_minus_1.items():
             jaccard =  len(nodeset_t_minus_1 & nodeset_t)/float(len((nodeset_t_minus_1 | nodeset_t))) 
             overlap_graph.add_edge(l_t_minus_1, l_t, weight=jaccard)
 
-    #logging.debug("overlap_graph nodes: %s", overlap_graph.nodes())
-    #logging.debug("overlap_graph edges: %s", overlap_graph.edges(data=True))
 
-    max_overlap_digraph = nx.DiGraph() # each label at t-1  and at t is a vertex in
-        # this bi-partite graph and a directed edge implies the max overlap with the
-        # other side (see comment at the beg of this function)
+    max_overlap_digraph = nx.DiGraph() 
+        # each label at t-1  and at t is a vertex in this bi-partite graph and 
+	# a directed edge implies the max overlap with the other side. 
 
     for v in overlap_graph.nodes():
         # find the nbr with max weight
         maxwt_nbr = max([(nbrs[0],nbrs[1]['weight']) for nbrs in overlap_graph[v].items()],
             key=operator.itemgetter(1))[0]
         max_overlap_digraph.add_edge(v, maxwt_nbr)
-
-    
-    #logging.debug("max_overlap_digraph nodes: %s", max_overlap_digraph.nodes())
-    #logging.debug("max_overlap_digraph edges %s", max_overlap_digraph.edges())
-
-
-    #logging.debug("out_degrees in max_overlap_digraph: %s",
-    #    str(max_overlap_digraph.out_degree()))
-
-    #logging.debug("in_degrees in max_overlap_digraph: %s",
-    #    str(max_overlap_digraph.in_degree()))
 
     matched_label_dict = {} # key = node, value = new label
     for l_t in nodesets_per_label_t.keys():
@@ -221,7 +207,6 @@ def match_labels(label_dict, prev_label_dict):
         for n in nodesets_per_label_t[l_t]:
             matched_label_dict[n] = best_matched_label
 
-    #logging.debug("matched_label_dict %s", str(matched_label_dict))
     return matched_label_dict
 
 
