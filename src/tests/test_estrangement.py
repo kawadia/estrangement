8bcd38cbb97fa67f43a934d0b174cf0d5aece448
dabideen <dabideen@bbn.com>
2012-06-26 17:05:10 -0400

    Added more unit test. Will revist to add test for ERA later.

--- src/tests/test_estrangement.py
+++ src/tests/test_estrangement.py
@@ -5,6 +5,14 @@ import os
 sys.path.append(os.getcwd() + "/..")
 import estrangement
 
+class opt:
+        resolution = 0.2
+        gap_proof_estrangement = True
+        delta = 0
+        tolerance = 0.4
+        precedence_tiebreaking = True
+
+
 class test_estrangement:
 	def setUp(self):
 	        self.g0 = nx.Graph()
@@ -13,13 +21,18 @@ class test_estrangement:
 		self.g3 = nx.Graph()
 		self.g4 = nx.Graph()
 		self.g5 = nx.Graph()
-		self.g5 = nx.Graph()
+		self.g6 = nx.Graph()
+		self.g7 = nx.Graph()
+		self.g8 = nx.Graph()
         	self.g0.add_edges_from([(1,2,{'weight':2}),(1,3,{'weight':1}),(2,4,{'weight':1})])
         	self.g1.add_edges_from([(1,4,{'weight':1}),(2,3,{'weight':1}),(3,4,{'weight':1})])
         	self.g2.add_edges_from([(1,2,{'weight':2}),(2,3,{'weight':1}),(3,4,{'weight':1})])
 		self.g3.add_edges_from([(1,2,{'weight':2})])
 		self.g4.add_edges_from([(1,2,{'weight':2}),(3,4,{'weight':1})])	
         	self.g5.add_edges_from([(1,2,{'weight':2}),(1,3,{'weight':1}),(2,3,{'weight':1}),(2,4,{'weight':1})])
+		self.g7.add_edges_from([(1,2,{'weight':1}),(2,3,{'weight':1}),(3,4,{'weight':1}),(4,5,{'weight':1}),(5,6,{'weight':1}),(6,1,{'weight':1})])
+		self.g8.add_edges_from([(1,2,{'weight':1}),(2,3,{'weight':1}),(1,3,{'weight':1}),(3,4,{'weight':1}),(4,5,{'weight':1}),(4,6,{'weight':1}),(5,7,{'weight':1}),(6,7,{'weight':1}),(8,9,{'weight':1}),(8,10,{'weight':1}),(9,10,{'weight':1})]
+)
 		self.label_dict1 = {1:'a',2:'a',3:'b',4:'b',5:'c',6:'c'}	
 		self.label_dict2 = {1:'a',2:'b',3:'b',4:'b',5:'c',6:'c'}	
 		self.label_dict3 = {1:'a',2:'b',3:'c',4:'d',5:'e',6:'f'}	
@@ -58,3 +71,18 @@ class test_estrangement:
 		print(self.g6.edges())
                 GM = nx.isomorphism.GraphMatcher(self.g4,self.g6)
                 assert GM.is_isomorphic()
+
+	def test_repeated_runs(self):
+		dictPartition,dictQ,DictE,DictF = estrangement.repeated_runs(self.g8,opt,1,self.g8,3)
+		print(dictPartition[0])
+		print(dictPartition[1])
+		print(dictPartition[2])
+		print(DictF[0])
+		print(DictF[1])
+		print(DictF[2])
+		# if the paritions are different the values of F will differ
+		assert DictF[0] != DictF[1] or DictF[1] != DictF[2] or DictF[0] != DictF[2]		
+
+	
+
+
