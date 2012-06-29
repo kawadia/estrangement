bf2ad8f4a6edfa3697a64caf85e2dc116d52cfc2
dabideen <dabideen@bbn.com>
2012-06-28 23:46:43 -0400

    removing use of opt, in progress...

--- src/postpro.py
+++ src/postpro.py
@@ -55,18 +55,7 @@ markers = [
 
 
 
-def parse_args():
-    """@brief parse cmd line and conf file options 
-    @retval opt as returned by argparseparse_args"""
-    # read in options from cmdline and conffile
-    usage="""usage: %prog [options] (--help for help)\n"""
-
-    parser = argparse.ArgumentParser(description="Analysis and visualization of Impression Propagation Algorithm", fromfile_prefix_chars='@')
-    visualoptions.add_options(parser)
-    opt = parser.parse_args(['@postpro.conf'])
-    #check_options(opt, parser)
-    return opt
-
+#SD: todo: I think only delta is used from here. can simply if so. 
 
 def parseOptions():
     # read the options dict for each run
@@ -87,7 +76,7 @@ def parseOptions():
 
 
 
-def plot_by_param(dictX, dictY, opt, fname=None, listLinestyles=None, xlabel="",
+def plot_by_param(dictX, dictY, opt,opt_linewidth=2.0,opt_markersize=15,label_fontsize=20,xfigsize=16.0,yfigsize=12.0,fontsize=28, fname=None, listLinestyles=None, xlabel="",
     ylabel="", title="", xscale='linear', yscale='linear', dictErr=None,
     display_on=False):
     """
@@ -95,20 +84,20 @@ def plot_by_param(dictX, dictY, opt, fname=None, listLinestyles=None, xlabel="",
     dictY with key=label, val = iterable of Y values, 
     plots lines for all the labels on the same plot.  """
     pyplot.clf()
-    fig2 = pyplot.figure(figsize=(opt.xfigsize,opt.yfigsize))
+    fig2 = pyplot.figure(figsize=(xfigsize,yfigsize))
     ax2 = fig2.add_subplot(111)
-    ax2.set_title(title, fontsize=opt.fontsize)
-    ax2.set_xlabel(xlabel, fontsize=opt.fontsize)
-    ax2.set_ylabel(ylabel, fontsize=opt.fontsize)
+    ax2.set_title(title, fontsize=fontsize)
+    ax2.set_xlabel(xlabel, fontsize=fontsize)
+    ax2.set_ylabel(ylabel, fontsize=fontsize)
 
     ax2.set_xscale(xscale)
     ax2.set_yscale(yscale)
 
     xticklabels = pyplot.getp(pyplot.gca(), 'xticklabels')
-    pyplot.setp(xticklabels, fontsize=opt.label_fontsize)
+    pyplot.setp(xticklabels, fontsize=label_fontsize)
 
     yticklabels = pyplot.getp(pyplot.gca(), 'yticklabels')
-    pyplot.setp(yticklabels, fontsize=opt.label_fontsize)
+    pyplot.setp(yticklabels, fontsize=label_fontsize)
     pyplot.hold(True)
     
     line_dict = {} # key = label, val = pyplot line object
@@ -134,14 +123,14 @@ def plot_by_param(dictX, dictY, opt, fname=None, listLinestyles=None, xlabel="",
             line_dict[label] = pyplot.errorbar(
                 arrayX, arrayY, yerr=[arrayErr, numpy.zeros(len(arrayErr))],
                 fmt=fmt,
-                label="%s"%str(label), linewidth=opt.linewidth,
-                elinewidth=opt.linewidth / 2.0,
-                markersize=opt.markersize)
+                label="%s"%str(label), linewidth=opt_linewidth,
+                elinewidth=opt_linewidth / 2.0,
+                markersize=opt_markersize)
         else:
             line_dict[label] = pyplot.plot(
                 arrayX, arrayY, fmt,
-                label="%s"%str(label), linewidth=opt.linewidth, 
-                markersize=opt.markersize)
+                label="%s"%str(label), linewidth=opt_linewidth, 
+                markersize=opt_markersize)
 
     # for the choosing delta plot only          
     #pyplot.ylim(ymin=-0.005)        
@@ -160,7 +149,7 @@ def plot_by_param(dictX, dictY, opt, fname=None, listLinestyles=None, xlabel="",
     
     if fname is not None:
         pyplot.savefig('%s'%fname)
-    if display_on is True or opt.display_on is True:
+    if display_on is True:
         pyplot.show()
 
     return ax2
@@ -458,7 +447,7 @@ def preprocess_temporal_communities(nodes_of_interest=[]):
     return node_index_dict, t_index_dict, label_index_dict, labels_of_interest_dict
 
 
-def plot_temporal_communities(nodes_of_interest=[]):
+def plot_temporal_communities(nodes_of_interest=[],deltas_to_plot=[],tiled_figsize='(36,16)',manual_colormap=None,label_cmap='Paired',opt_frameon=True,xlabel="Time",ylabel="Node id",label_fontsize=20,show_title=True,opt_fontsize=28,colorbar=True,show_yticklabels=False,nodelabel_func=None,xtick_separation=5,snapshotlabel_func=None,opt_wspace=0.2,opt_bottom=0.1,image_extension="svg",opt_dpi=200):
     """ the tiled plots for all the runs 
     
     if nodes_of_interest is not an empty list then show egocentric view of the
@@ -486,9 +475,9 @@ def plot_temporal_communities(nodes_of_interest=[]):
 
 
     # now make the tiled plots for all the runs
-    deltas_to_plot = eval(opt.deltas_to_plot)
+    #deltas_to_plot = eval(deltas_to_plot)
 
-    fig1 = pylab.figure(figsize=eval(opt.tiled_figsize))
+    fig1 = pylab.figure(figsize=eval(tiled_figsize))
     numRows = 1
     if len(deltas_to_plot) is 0:
         numCols = len(listOptionsDict) 
@@ -499,8 +488,8 @@ def plot_temporal_communities(nodes_of_interest=[]):
         numCols += 1 # +1 for the merged network
 
     #http://matplotlib.sourceforge.net/api/colors_api.html#matplotlib.colors.ListedColormap
-    if opt.manual_colormap is not None:
-        manual_colormap = eval(opt.manual_colormap)
+    if manual_colormap is not None:
+        manual_colormap = eval(manual_colormap)
         if len(manual_colormap) != len(label_index_dict):
             logging.error("Error: Length of manual_colormap does not match that of label_index_dict")
             logging.error("manual_color_map = %s, len=%d", str(manual_colormap), len(manual_colormap))
@@ -510,7 +499,7 @@ def plot_temporal_communities(nodes_of_interest=[]):
         cmap = matplotlib.colors.ListedColormap([manual_colormap[l] for l in sorted(manual_colormap.keys())],
             name='custom_cmap', N=None)
     else:
-        cmap=pylab.cm.get_cmap(opt.label_cmap, len(label_index_dict))
+        cmap=pylab.cm.get_cmap(label_cmap, len(label_index_dict))
 
     print "Hello, I am here"
 
@@ -538,18 +527,18 @@ def plot_temporal_communities(nodes_of_interest=[]):
 
         if prev_ax is not None:
             ax1 = fig1.add_subplot(numRows, numCols, plotNum, sharex=prev_ax,
-                sharey=prev_ax, frameon=opt.frameon)
+                sharey=prev_ax, frameon=opt_frameon)
             ax1.get_yaxis().set_visible(False)
         else:    
-            ax1 = fig1.add_subplot(numRows, numCols, plotNum, frameon=opt.frameon)
+            ax1 = fig1.add_subplot(numRows, numCols, plotNum, frameon=opt_frameon)
             prev_ax = ax1
 
         #ax1.xaxis.set_major_locator(majorLocator)
-        ax1.set_xlabel(opt.xlabel, fontsize=opt.label_fontsize)
+        ax1.set_xlabel(xlabel, fontsize=label_fontsize)
         if plotNum == 1:
-            ax1.set_ylabel(opt.ylabel, fontsize=opt.label_fontsize)
-        if opt.show_title is True:
-            ax1.set_title("$\delta$=%s"%delta , fontsize=opt.fontsize)
+            ax1.set_ylabel(ylabel, fontsize=label_fontsize)
+        if show_title is True:
+            ax1.set_title("$\delta$=%s"%delta , fontsize=opt_fontsize)
         #ax1.set_title(opt.title, fontsize=opt.fontsize)
         
         # Is not working with this version of mpl
@@ -601,7 +590,7 @@ def plot_temporal_communities(nodes_of_interest=[]):
         # specifying x and y leads to last row and col of Labels beting omitted from plotting
         #pylab.pcolor(x, y, Labels_masked, cmap=pylab.cm.get_cmap(opt.label_cmap), alpha=opt.alpha)
         
-        if opt.colorbar is True:
+        if colorbar is True:
             levels = numpy.unique(Labels_masked)
             cb = pylab.colorbar(ticks=levels)
             reverse_label_index_dict = dict([(v,k) for (k,v) in label_index_dict.items()])
@@ -627,34 +616,34 @@ def plot_temporal_communities(nodes_of_interest=[]):
         ylocs = sorted(node_index_dict.values(), key=int)
         ylabs = sorted(node_index_dict.keys(), key=node_index_dict.get)
 
-        if opt.show_yticklabels is False:
+        if show_yticklabels is False:
             ax1.set_yticklabels([])
         else:    
-            if opt.nodelabel_func is not None:
-                nodelabel_dict = eval(opt.nodelabel_func+'()')
+            if nodelabel_func is not None:
+                nodelabel_dict = eval(nodelabel_func+'()')
                 node_labels = [str(nodelabel_dict[c]) for c in ylabs]
                 pylab.yticks(ylocs, node_labels, fontsize=10, rotation=15)
             else:
                 pylab.yticks(ylocs, ylabs, fontsize=10)
 
         # show every 5th label on the x axis
-        xlocs = [x for x in sorted(t_index_dict.values(), key=int) if x%opt.xtick_separation == 0]
+        xlocs = [x for x in sorted(t_index_dict.values(), key=int) if x%xtick_separation == 0]
         #xlabs = sorted(t_index_dict.keys(), key=t_index_dict.get)
         #xlocs = [x for x in sorted(ax1.xaxis.get_ticklocs()) if x%5 == 0]
         logging.debug("xlocs:%s", str(xlocs))
         xlabs = [t_index_to_label_dict[x] for x in xlocs]
         logging.debug("xlabs:%s", str(xlabs))
 
-        if opt.snapshotlabel_func is not None:
-            snapshotlabel_dict = eval(opt.snapshotlabel_func+'()')
+        if snapshotlabel_func is not None:
+            snapshotlabel_dict = eval(snapshotlabel_func+'()')
             snapshot_labels = [snapshotlabel_dict[t] for t in xlabs]
             pylab.xticks(xlocs, snapshot_labels, fontsize=11, rotation=75)
         else:
             pylab.xticks(xlocs, xlabs, fontsize=11)
 
         suffix=''
-        if opt.nodelabel_func is not None:
-            nodelabel_dict = eval(opt.nodelabel_func+'()')
+        if nodelabel_func is not None:
+            nodelabel_dict = eval(nodelabel_func+'()')
             suffix='-'.join([str(nodelabel_dict[n]) for n in nodes_of_interest])
         else:    
             suffix='-'.join([str(n) for n in nodes_of_interest])
@@ -663,11 +652,11 @@ def plot_temporal_communities(nodes_of_interest=[]):
         ax1.set_xlim((min(xvals), max(xvals)))
         # end for delta, runs_iter in itertools.groupby(listOptionsDict,operator.itemgetter('delta')):
 
-    fig1.subplots_adjust(wspace=opt.wspace, bottom=opt.bottom)
+    fig1.subplots_adjust(wspace=opt_wspace, bottom=opt_bottom)
     
-    pylab.savefig('dynconsuper%s.%s'%(suffix,opt.image_extension), dpi=opt.dpi)
+    pylab.savefig('dynconsuper%s.%s'%(suffix,image_extension), dpi=opt_dpi)
     # svg viewers are slow, also save pdf
-    pylab.savefig('dynconsuper%s.%s'%(suffix,'pdf'), dpi=opt.dpi)
+    pylab.savefig('dynconsuper%s.%s'%(suffix,'pdf'), dpi=opt_dpi)
     #if opt.display_on is True:
     pylab.show()
 
@@ -693,11 +682,11 @@ def main():
 
 if __name__ == '__main__':
 
-    global opt
+#    global opt
+#
+#   opt = parse_args()
 
-    opt = parse_args()
-
-    print opt
+#    print opt
     
     logging.basicConfig(level=getattr(logging, opt.loglevel.upper(), None))
 
