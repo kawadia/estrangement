bec6344dd3943fc0c0028db262af90d7a77175c8
Vikas Kawadia <kawadia@gmail.com>
2012-07-09 16:32:45 -0400

    remove runner scrips file from master

--- runner_scripts/watch-progress.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-
-# $Id$
-
-watch -n 2 'files="$(find . -name 'simulation.inprogress' -o -name 'simulation.done')" ; if [ -n "${files}" ] ; then tail -n 1 ${files} | tail -n $(( $(tput lines) - 2 )) ; else echo "no status" ; fi'
