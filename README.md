e5c0c40f7ef13202c71a0487e42abb5c24539575
dabideen <dabideen@bbn.com>
2012-07-10 16:18:36 -0400

    cleaned up the sample script.

--- README.md
+++ README.md
@@ -4,3 +4,26 @@ estrangement
 Method to detect temporal communities in time-varying complex networks
 
 More details in http://arxiv.org/abs/1203.5126
+
+===Dependencies===
+
+====Ubuntu====
+python-networkx
+python-scipy
+python-numpy
+python-matplotlib
+
+
+====Standard====
+types
+logging
+math
+os
+pprint
+random 
+collections
+sys
+operator
+
+
+
--- src/sample/sample.py
+++ src/sample/sample.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""Sample script demonstrating the use of the Estrangement library to plot temporal communities. """
+
+__author__ = """\n""".join(['Vikas Kawadia (vkawadia@bbn.com)',
+                            'Sameet Sreenivasan <sreens@rpi.edu>',
+                            'Stephen Dabideen <dabideen@bbn.com>'])
+
+#   Copyright (C) 2012 by 
+#   Vikas Kawadia <vkawadia@bbn.com>
+#   Sameet Sreenivasan <sreens@rpi.edu>
+#   Stephen Dabideen <dabideen@bbn.com>
+#   All rights reserved. 
+#   BSD license. 
+
 
 import sys
 import os
@@ -12,17 +26,24 @@ import lpa
 import string
 import estrangement
 
-
+# use argparse to parse command-line arguments using optionsadder.py
 opt = optionsadder.parse_args()
+
+# set the values of delta for which to create plots
 deltas = [0.01,0.025,0.05,1.0]
 
+# dictionary to pass the simulation output to the plot function
 matched_labels_dict = {}
 
+# Prompt the user for a name of the experiment. A folder is created in 
+# the current working directory and all the files from the experiment will
+# be placed in this file. 
 exp_name = raw_input("Please enter experiment name:")
 if(not os.path.exists(exp_name)):
     os.mkdir(exp_name)
 os.chdir(exp_name)
 
+# Prompt the user for the path to the dataset
 path_to_data=raw_input("Please enter path to data:")
 if(not os.path.isdir(path_to_data)):
      sys.exit("data folder %s does not exist"%path_to_data)
@@ -35,19 +56,24 @@ for d in deltas:
 		with open("task_delta_" + str(d) + "/matched_labels.log", 'r') as ml:
                 	matched_labels = ml.read()
 			matched_labels_dict[str(d)] = eval(matched_labels)
-#			print(matched_labels)
 			continue
+
 	# run the simulation if the matched_labels.log file does not exist or the user specifies this is desired
 	matched_labels = estrangement.ERA(dataset_dir=path_to_data,delta=d,increpeats=opt.increpeats,minrepeats=opt.minrepeats)
         matched_labels_dict[str(d)] = matched_labels
 	matched_label_file = open("task_delta_" + str(d) +"/matched_labels.log", 'w')
         matched_label_file.write(str(matched_labels))
-#	print(str(matched_labels))
 	matched_label_file.close()
 
- 
+# plot the temporal communities 
 postpro.plot_temporal_communities(matched_labels_dict)
 os.chdir("..")
+
+
+# to plot other parameters, set write_stats=True in estrangement.ERA() 
+# and use postpro.plot_function(). For example,
+# postpro.plot_function(['Estrangement'])
+# postpro.plot_function(['ierr','feasible'])
 
 
 
